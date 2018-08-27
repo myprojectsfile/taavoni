4327c7a17289e48b3b0a9ccbcb5ad26a5aba2c7a
ahmadi <ahmadi80@gmail.com>
2018-08-27 20:17:43 +0430

    پس از ایجاد قسمت جستجوی کاربر

--- src/app/auth/route.claims.ts
+++ src/app/auth/route.claims.ts
@@ -33,7 +33,7 @@ export default {
         {
             "routeUrl": "/useradmin",
             "signInRequired": "true",
-            "claims": ["useradmin"]
+            "claims": ["userAdmin"]
         }
     ]
 }
