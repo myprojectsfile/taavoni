f2e7f0356d22f1e8407bbf7b940628a77bc53741
topdevelopers <my.solutions.asset@gmail.com>
2019-05-03 18:39:07 +0430

    1-change image slider 2-change navbar padding uppon scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  constructor(public authService: AuthService) {
-
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    $(window).scroll(() => {
+      const position = $(window).scrollTop();
+      if (position >= 200) {
+        $('.navbar').addClass('navbar-change');
+      } else {
+        $('.navbar').removeClass('navbar-change');
+      }
+    });
   }
+  constructor(public authService: AuthService) {}
 
   userHasClaim(claim: string) {
     return this.authService.userHasClaim(claim);
@@ -23,5 +31,4 @@ export class AppComponent {
   userIsAuthenticated() {
     return this.authService.isAuthenticated();
   }
-
 }
