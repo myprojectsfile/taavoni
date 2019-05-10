71ca98463d4f80e1f39be8d73f1dcc2300548231
topdevelopers <my.solutions.asset@gmail.com>
2019-05-11 02:14:45 +0430

    ریدایرکت به صفحه اصلی برنامه پس از ورود

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Location } from '@angular/common';
+// import { Location } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { JwtHelperService } from '@auth0/angular-jwt';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
-
   username: string;
   password: string;
   errorMessage: string = null;
 
-  constructor(private authService: AuthService, private location: Location, private toastr: ToastrService, private jwt: JwtHelperService) {
-
-  }
+  constructor(
+    private authService: AuthService,
+    // private location: Location,
+    private toastr: ToastrService,
+    private jwt: JwtHelperService,
+    private router: Router
+  ) {}
 
   login() {
     this.authService.login(this.username, this.password).subscribe(
@@ -28,15 +30,14 @@ export class LoginComponent implements OnInit {
         const isAuthenticted = this.authService.isAuthenticated();
         const tokenPayload: any = this.authService.tokenPayload();
         this.toastr.success('به سامانه تعاونی کارکنان بندر بوشهر خوش آمدید');
-        this.location.back();
-      }
-      , errRes => {
+        this.router.navigate(['']);
+        // this.location.back();
+      },
+      errRes => {
         console.log(errRes);
         this.errorMessage = errRes.error.message;
       }
     );
   }
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
